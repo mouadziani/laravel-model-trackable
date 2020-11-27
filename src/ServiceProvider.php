cbb963efda6707bf7cdcdd5ea636be22089ced49
MouadZIANI <mouad.ziani1997@gmail.com>
2020-11-27 02:34:49 +0100

    feat(pkg): added serviceProvider and enable publish migrations

---
+++ src/ServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace LaravelModelTrackable;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+/**
+ * Service provider
+ */
+class ServiceProvider extends BaseServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $timestamp = date('Y_m_d_His', time());
+        $this->publishes([
+            __DIR__ . "/../migrations/create_logs_table.php" => database_path("/migrations/{$timestamp}_create_logs_table.php"),
+        ], 'migrations');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
